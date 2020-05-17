97060e5d6013be717f8837bb01d13838c19f273b
Intercisa <sipiczkibarna@gmail.com>
2020-05-17 13:52:42 +0200

    window sliding

---
+++ CommonInterviewQuestons/WindowSliding/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace WindowSliding
+{
+
+    /*
+     * a sliding window is a sub-list that runs over an underlying collection
+     * [a b c d e f g h]
+     * 
+     *  [a b c]
+     *     [b c d]
+     *        [c d e]
+     *           [d e f]
+     *              [e f g]
+     *                 [f g h]
+     *                 
+     *                 
+     * The problem will involve a data structure that is ordered and iterable like an array or a string
+     * You are looking for some subrange in that array/string, like a longest, shortest or target value.
+     * There is an apparent naive or brute force solution that runs in O(N²), O(2^N) or some other large time complexity.
+     * looking for is often some kind of optimal, like the longest sequence 
+     * or shortest sequence of something that satisfies a given condition exactly.
+     * 
+     * Two type: 
+     *  Fixed window length k:
+     *  Two pointers + criteria
+     * 
+     * 1, find max averge  -> Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. 
+     * And you need to output the maximum average value. >> first type
+     * 
+     * 2. Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. 
+     * You can only see the k numbers in the window. Each time the sliding window moves right by one position. 
+     * Your job is to output the median array for each window in the original array.
+     * 
+     * 3. Given an array of n positive integers and a positive integer s, 
+     * find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn’t one, return 0 instead.
+     * 
+     * 4. Given a string, find the length of the longest substring T that contains at most k distinct characters.
+     * [e,c,e,b,a], k = 2, the first window is [e, c, e], the next starting index should be 2
+     * [e,c,c,b,a], k = 2, the first window is [e, c, c], the next starting index should be 1
+     * 
+     * 
+     *  https://medium.com/@zengruiwang/sliding-window-technique-360d840d5740
+     * 
+     */
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SlidingImp sliding = new SlidingImp();
+            Console.WriteLine(sliding.FindMaxAverage(new int[] { 1,12,-5,-6,50,3}, 4));
+
+
+            Console.WriteLine(sliding.LengthOfLongestSubstringKDistinct("eceba", 2));
+
+
+        }
+    }
+}
---
+++ CommonInterviewQuestons/WindowSliding/SlidingImp.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowSliding
+{
+    class SlidingImp
+    {
+        //1,
+        public double FindMaxAverage(int[] nums, int k) {
+            int sum = 0, maxSum = int.MinValue;
+
+            for (int i = 0; i < k; i++)
+                sum += nums[i];
+
+            for (int i = k; i <= nums.Length; i++)
+            {
+                maxSum = Math.Max(maxSum, sum);
+
+                if (i == nums.Length) break;
+                sum -= nums[i - k];
+                sum += nums[i];
+            }
+
+            return maxSum * 1.0 / k; //* 1.0 -> double and /k because average
+        
+        }
+
+        //2,
+        public double[] MedianSlidingWindow(int[] nums, int k) {
+            int N = nums.Length;
+            double[] res = new double[N - k + 1];
+            int[] sorted = new int[k];
+            //construct the first sorted window
+
+            for (int i = 0; i < k; i++)
+                sorted[i] = nums[i];
+                Array.Sort(sorted);
+
+                for (int i = k; i <= N ; i++)
+                {
+                //calculate the median, k / 2 is the right median (k - 1)/2 is the left median
+                res[i - k] = sorted[k / 2] * 0.5 + sorted[(k - 1)/2] * 0.5;
+                if (i == N) break;
+
+                //remove the first element in the window
+                Remove(sorted, nums[i - k]);
+
+                //insert the next element to the window 
+                insert(sorted, nums[i]);
+                }
+
+            return res;
+        }
+
+        private void insert(int[] sorted, int x)
+        {
+            int i = 0;
+            while (i < sorted.Length && sorted[i] < x)
+                i++;
+
+            //shift all elements beyond x to left 1 position
+            while (i < sorted.Length - 1) { 
+                sorted[i] = sorted[i + 1];
+                i++;
+            }  
+        }
+
+        private void Remove(int[] sorted, int x)
+        {
+            //insert x into the right position
+            int i = sorted.Length - 2;
+            while (i >= 0 && sorted[i] > x) {
+                sorted[i + 1] = sorted[i];
+                i--;
+            }
+            sorted[i + 1] = x;
+        }
+
+        //3,
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            int N = nums.Length, minLen = int.MaxValue, i = 0, sum = 0;
+
+            for (int j = 0; j < N; j++)
+            {
+                sum += nums[j];
+
+                while (sum >= s)
+                {
+
+                    minLen = Math.Min(minLen, j - i + 1);
+                    sum -= nums[i++];
+
+                }
+            }
+            return minLen == int.MaxValue ? 0 : minLen;
+        }
+
+
+        //4, 
+        public int LengthOfLongestSubstringKDistinct(string s, int k) {
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int maxLen = 0;
+            for (int i = 0, j = 0; j < s.Length; j++)
+            {
+                //map.put(s.charAt(j), j);
+                dict[s[j]] = j;
+                if (dict.Count > k) {
+                    while (i < j && dict[s[i]] != i) {
+                        i++;
+                    }
+                    dict.Remove(s[i++]);
+                }
+                maxLen = Math.Max(maxLen, j - i + 1);
+            }
+            return maxLen;
+        }
+
+    }
+}
