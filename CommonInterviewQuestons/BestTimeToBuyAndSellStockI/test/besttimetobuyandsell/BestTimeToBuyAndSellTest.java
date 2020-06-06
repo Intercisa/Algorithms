79cdb01f81c7e3731811a0928d6211b182bd0a2f
Intercisa <sipiczkibarna@gmail.com>
2020-06-06 09:41:36 +0200

    fixing some problems with the code

--- CommonInterviewQuestons/BestTimeToBuyAndSellStockI/test/besttimetobuyandsell/BestTimeToBuyAndSellTest.java
+++ CommonInterviewQuestons/BestTimeToBuyAndSellStockI/test/besttimetobuyandsell/BestTimeToBuyAndSellTest.java
@@ -8,11 +8,20 @@ class BestTimeToBuyAndSellTest {
 
 	@Test
 	void test_1() {
-			assertEquals(5, new BestTimeToBuyAndSell().getMaxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+		assertEquals(5, new BestTimeToBuyAndSell().getMaxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+	}
+	@Test
+	void test_2() {
+		assertEquals(1, new BestTimeToBuyAndSell().getMaxProfit(new int[] {1,2}));
 	}
 	
 	@Test
-	void test_2() {
+	void test_3() {
 		assertEquals(8, new BestTimeToBuyAndSell().getMaxProfitDP(new int[] {7,4,5,6,9,12,1}));
+	}
+	
+	@Test
+	void test_4() {
+		assertEquals(1, new BestTimeToBuyAndSell().getMaxProfitDP(new int[] {1,2}));
 	}
 }
