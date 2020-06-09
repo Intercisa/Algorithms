f2b2846ad8c20660fb669f76fc1c092cd9b98722
Intercisa <sipiczkibarna@gmail.com>
2020-06-09 10:33:51 +0200

    best time to sell II and with cooldown

---
+++ CommonInterviewQuestons/BestTimeToBuyAndSellStockII/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+/*
+ *
+ * Given an array of integers prices, where each element prices[i] represents the stock price on a day i, 
+ * create a function that returns the maximum profit that we can get by making transactions. 
+ * A transaction is when we buy and sell a stock.
+ * Note that you can do any number of transactions you want, but you have to buy a stock before selling it, 
+ * and you have to end the actual transaction before starting 
+ * 
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
