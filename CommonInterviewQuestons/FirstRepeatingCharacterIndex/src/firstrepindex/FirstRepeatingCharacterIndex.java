dbeba2fff101a7712f3b83c1bd6782ca59158e60
Sipiczki Barna <49206738+Intercisa@users.noreply.github.com>
2020-07-20 12:50:05 +0200

    Update FirstRepeatingCharacterIndex.java
    naming correction

--- CommonInterviewQuestons/FirstRepeatingCharacterIndex/src/firstrepindex/FirstRepeatingCharacterIndex.java
+++ CommonInterviewQuestons/FirstRepeatingCharacterIndex/src/firstrepindex/FirstRepeatingCharacterIndex.java
@@ -6,33 +6,33 @@ import java.util.LinkedHashMap;
 public class FirstRepeatingCharacterIndex {
 	public int findFirstIndex(String str) {
 		
-		HashMap<Character, IndexHash<Integer, Integer>> hash = 
-				new LinkedHashMap<Character, FirstRepeatingCharacterIndex.IndexHash<Integer,Integer>>();
+		HashMap<Character, IndexHash<Integer, Integer>> map = 
+				new LinkedHashMap<Character, FirstRepeatingCharacterIndex.IndexMap<Integer,Integer>>();
 		
 		for (int i = 0; i < str.length(); i++) {
 		
 			if(!hash.containsKey(str.charAt(i))) {
-				IndexHash<Integer, Integer> temp = new IndexHash<Integer, Integer>();
+				IndexMap<Integer, Integer> temp = new IndexMap<Integer, Integer>();
 				temp.index =i;
 				temp.num=1;
-				hash.put(str.charAt(i),temp);
+				map.put(str.charAt(i),temp);
 			}else {
-				hash.get(str.charAt(i)).num++;
+				map.get(str.charAt(i)).num++;
 			}
 		}
 
-	 	return hash.values().stream().filter( i -> i.num == 1)
+	 	return map.values().stream().filter( i -> i.num == 1)
 		.findFirst().get().index;
 
 	}
 	
-	private class IndexHash<K, V>{
+	private class IndexMap<K, V>{
 		private K num;
 		private V index;
 		
 		@Override
 		public String toString() {
-			return String.format("IndexHash [num=%s, index=%s]", num, index);
+			return String.format("IndexMap [num=%s, index=%s]", num, index);
 		}
 		
 	}
